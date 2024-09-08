5723b52b7644927b75b953826eb1abc056fd81f7
matheus kikuti <matheus.kikuti@rappi.com>
2024-09-07 22:53:34 -0300

    Criando o CRUD de produtos

--- ms-tcAtelie-auth/src/main/java/tcatelie/microservice/auth/controller/ProdutoController.java
+++ ms-tcAtelie-auth/src/main/java/tcatelie/microservice/auth/controller/ProdutoController.java
@@ -5,10 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tcatelie.microservice.auth.dto.request.ProdutoRequestDTO;
-import tcatelie.microservice.auth.model.Produto;
+import tcatelie.microservice.auth.dto.response.ProdutoResponseDTO;
 import tcatelie.microservice.auth.service.ProdutoService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/produto")
@@ -34,6 +32,38 @@ public class ProdutoController {
         } catch (Exception e){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body("Ocorreu um erro na listagem de produtos");
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<String> atualizarProduto(@RequestBody ProdutoRequestDTO produto, @RequestParam Integer id){
+        try{
+            service.atualizarProduto(produto, id);
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body("Produto atualizado com sucesso.");
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Ocorreu um erro durante a atulização do produto.");
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarProduto(@RequestBody ProdutoResponseDTO produto){
+        try{
+            return service.buscarProduto(produto);
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Ocorreu um erro durante a busca do produto.");
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletarProduto(@PathVariable int id){
+        try{
+            service.deletarProduto(id);
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body("Produto deletado com sucesso.");
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Ocorreu um erro durante a deleção do produto.");
         }
     }
 }
--- ms-tcAtelie-auth/src/main/java/tcatelie/microservice/auth/service/ProdutoService.java
+++ ms-tcAtelie-auth/src/main/java/tcatelie/microservice/auth/service/ProdutoService.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 import tcatelie.microservice.auth.dto.request.ProdutoRequestDTO;
+import tcatelie.microservice.auth.dto.response.ProdutoResponseDTO;
 import tcatelie.microservice.auth.mapper.ProdutoMapper;
 import tcatelie.microservice.auth.model.Produto;
 import tcatelie.microservice.auth.repository.ProdutoRepository;
@@ -23,7 +24,6 @@ public class ProdutoService {
         this.repository = repository;
     }
 
-    @PostMapping
     public ResponseEntity<?> cadastrarProduto(ProdutoRequestDTO produtoRequest){
         try{
             Produto produto = mapper.toEntity(produtoRequest);
@@ -34,7 +34,6 @@ public class ProdutoService {
         }
     }
 
-    @GetMapping
     public ResponseEntity<?> listarProduto(){
         try{
             if(repository.findAll().isEmpty()){
@@ -49,18 +48,59 @@ public class ProdutoService {
         }
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Produto> buscarProduto(@PathVariable int id){
+    public ResponseEntity<Produto> buscarProduto(ProdutoResponseDTO produtoResponse){
         try{
-            Optional<Produto> produtoId = repository.findById(id);
-            return produtoId.map(produto -> ResponseEntity.status(HttpStatus.ACCEPTED)
-                    .body(produto)).orElseGet(() -> ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+            Optional<Produto> produtoById = repository.findById(produtoResponse.getId());
 
+            return produtoById.map(produto -> ResponseEntity.status(HttpStatus.ACCEPTED)
+                    .body(produto)).orElseGet(() -> ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
         } catch (Exception e){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
 
-    @PutMapping("/{id}")
-    public
+    public ResponseEntity<String> atualizarProduto(ProdutoRequestDTO produtoRequest, @RequestParam int id){
+        try{
+            Optional<Produto> produtoId = repository.findById(id);
+
+            if(produtoId.isPresent()){
+                Produto existingProduct = produtoId.get();
+
+                existingProduct.setNome(produtoRequest.getNome());
+                existingProduct.setPreco(produtoRequest.getPreco());
+                existingProduct.setDescricao(produtoRequest.getDescricao());
+                existingProduct.setMaterial(produtoRequest.getMaterial());
+                existingProduct.setDimensao(produtoRequest.getDimensao());
+                existingProduct.setUrl_produto(produtoRequest.getUrl_produto());
+                repository.save(existingProduct);
+
+                return ResponseEntity.status(HttpStatus.ACCEPTED)
+                        .body("Atualização de dados feito com sucesso.");
+            }else{
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body("Não foi possivel identificar o produto.");
+            }
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Ocorreu um erro durante a atualização do produto.");
+        }
+    }
+
+    public ResponseEntity<String> deletarProduto(@RequestParam int id){
+        try{
+            Optional<Produto> produtoId = repository.findById(id);
+
+            if(produtoId.isPresent()){
+                repository.deleteById(id);
+                return ResponseEntity.status(HttpStatus.ACCEPTED)
+                        .body("Produto deletado com sucesso.");
+            }else{
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body("Não foi possivel identificar o produto.");
+            }
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Ocorreu um erro durante a deleção do produto.");
+        }
+    }
 }
