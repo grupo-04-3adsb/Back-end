ad7afa8cba5ffcf02235371a0b38c061f91d1796
Claudio712005 <clausilvaaraujo11@gmail.com>
2024-09-13 21:22:57 -0300

    Implementação do Login para Admin e para usuário comum

--- ms-tcAtelie-auth/src/main/java/tcatelie/microservice/auth/controller/AuthenticationController.java
+++ ms-tcAtelie-auth/src/main/java/tcatelie/microservice/auth/controller/AuthenticationController.java
@@ -3,6 +3,7 @@ package tcatelie.microservice.auth.controller;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,10 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 import tcatelie.microservice.auth.dto.AuthenticationDTO;
 import tcatelie.microservice.auth.dto.RegisterDTO;
 import tcatelie.microservice.auth.dto.response.LoginResponseDTO;
+import tcatelie.microservice.auth.dto.response.UsuarioResponseDTO;
 import tcatelie.microservice.auth.model.Usuario;
 import tcatelie.microservice.auth.repository.UserRepository;
 import tcatelie.microservice.auth.service.JwtService;
 import tcatelie.microservice.auth.service.UsuarioService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("auth")
@@ -34,15 +38,53 @@ public class AuthenticationController {
 	@Autowired
 	private UsuarioService service;
 
-	@PostMapping("/login")
-	public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data) {
+	@PostMapping("/admin/login")
+	public ResponseEntity loginDashboard(@RequestBody @Valid AuthenticationDTO data) {
 		var emailSenha = new UsernamePasswordAuthenticationToken(data.getEmail(), data.getSenha());
 		var auth = this.authenticationManager.authenticate(emailSenha);
 
-		var token = jwtService.generateToken((Usuario) auth.getPrincipal());
+		var usuario = (Usuario) auth.getPrincipal();
 
-		return ResponseEntity.ok(new LoginResponseDTO(token));
+		boolean isUser = auth.getAuthorities().stream()
+				.anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_ADMIN"));
+
+		if (!isUser) {
+			return ResponseEntity.status(403).body("Acesso negado.");
+		}
+
+		var token = jwtService.generateToken(usuario);
+
+		ResponseEntity response = service.buscarUsuarioEmailSenha(data);
+
+		return ResponseEntity.ok(new LoginResponseDTO((UsuarioResponseDTO) response.getBody(), token));
 	}
+
+	@PostMapping("/customer/login")
+	public ResponseEntity<?> loginEcommerce(@RequestBody @Valid AuthenticationDTO data) {
+
+		var emailSenha = new UsernamePasswordAuthenticationToken(data.getEmail(), data.getSenha());
+		var auth = this.authenticationManager.authenticate(emailSenha);
+
+		var usuario = (Usuario) auth.getPrincipal();
+
+		boolean isUser = auth.getAuthorities().stream()
+				.anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_USER"));
+
+		boolean isAdmin = auth.getAuthorities().stream()
+				.anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_ADMIN"));
+
+		if (isAdmin || !isUser) {
+			return ResponseEntity.status(403).body("Acesso negado. Apenas clientes podem acessar.");
+		}
+
+		var token = jwtService.generateToken(usuario);
+
+		ResponseEntity response = service.buscarUsuarioEmailSenha(data);
+
+		return ResponseEntity.ok(new LoginResponseDTO((UsuarioResponseDTO) response.getBody(), token));
+	}
+
+
 
 	@PostMapping("/register")
 	public ResponseEntity<?> cadastrarProduto(@RequestBody @Valid RegisterDTO data) {
