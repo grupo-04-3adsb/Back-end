d570e84d7f9263b537f5a4a748469b78f7815225
Claudio712005 <clausilvaaraujo11@gmail.com>
2024-11-23 00:04:04 -0300

    Correção API google drive

--- src/main/resources/db/data/insert-categoria.sql
+++ src/main/resources/db/data/insert-categoria.sql
@@ -1,7 +1,7 @@
-INSERT INTO categoria (nome_categoria)
-VALUES ('Papelaria e Escritório'),
-       ('Festas e Eventos'),
-       ('Adesivos'),
-       ('Personalização e Brindes'),
-       ('Artesanato e Materiais Artísticos'),
-       ('Artigos para Datas Comemorativas');
+INSERT INTO categoria (nome_categoria, categoria_ativa)
+VALUES ('Papelaria e Escritório', true),
+       ('Festas e Eventos', true),
+       ('Adesivos', true),
+       ('Personalização e Brindes', true),
+       ('Artesanato e Materiais Artísticos', true),
+       ('Artigos para Datas Comemorativas', true);
--- src/main/resources/db/data/insert-subcategoria.sql
+++ src/main/resources/db/data/insert-subcategoria.sql
@@ -1,10 +1,10 @@
-INSERT INTO subcategoria (nome_subcategoria, descricao_subcategoria, fk_categoria)
-VALUES ('Agendas e Cadernos', 'Cadernos e agendas personalizadas e decoradas', 1),
-       ('Canecas', 'Canecas decorativas e personalizadas', 3),
-       ('Decoração para Festas', 'Decoração', 2),
-       ('Camisetas Temáticas', 'Camisetas com estampas personalizadas e de personagens', 3),
-       ('Decoração de Mesa para Festas', 'Itens decorativos para mesas de festas', 2),
-       ('Brindes Corporativos', 'Itens promocionais personalizados para empresas', 3),
-       ('Material para Scrapbook e Pintura', 'Material para criação de scrapbook e pintura', 4),
-       ('Decoração Natalina', 'Itens de decoração para o Natal', 5),
-       ('Brinquedos Educativos', 'Brinquedos que estimulam o aprendizado', 6);
+INSERT INTO subcategoria (nome_subcategoria, descricao_subcategoria, fk_categoria, subcategoria_ativa)
+VALUES ('Agendas e Cadernos', 'Cadernos e agendas personalizadas e decoradas', 1, true),
+       ('Canecas', 'Canecas decorativas e personalizadas', 3, true),
+       ('Decoração para Festas', 'Decoração', 2, true),
+       ('Camisetas Temáticas', 'Camisetas com estampas personalizadas e de personagens', 3, true),
+       ('Decoração de Mesa para Festas', 'Itens decorativos para mesas de festas', 2, true),
+       ('Brindes Corporativos', 'Itens promocionais personalizados para empresas', 3, true),
+       ('Material para Scrapbook e Pintura', 'Material para criação de scrapbook e pintura', 4, true),
+       ('Decoração Natalina', 'Itens de decoração para o Natal', 5, true),
+       ('Brinquedos Educativos', 'Brinquedos que estimulam o aprendizado', 6, true);
