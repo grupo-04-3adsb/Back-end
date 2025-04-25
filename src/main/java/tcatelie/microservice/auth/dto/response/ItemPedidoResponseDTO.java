7a352f6c11b626ec85fab822035230d2a9d4079b
Claudio712005 <clausilvaaraujo11@gmail.com>
2025-04-25 16:14:49 -0300

    Ajuste DTO e endpoint pedidos

--- src/main/java/tcatelie/microservice/auth/dto/response/ItemPedidoResponseDTO.java
+++ src/main/java/tcatelie/microservice/auth/dto/response/ItemPedidoResponseDTO.java
@@ -15,23 +15,23 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class ItemPedidoResponseDTO {
-    private Integer id;
-    @Schema(description = "Quantidade de produtos", example = "10")
-    private Integer quantidade;
-    @Schema(description = "Valor unitário do produto", example = "10.00")
-    private Double valor;
-    @Schema(description = "Valor pago por este item com frete, desconto, valor unitário * qauntidade + custo da personalizacao", example = "100.00")
-    private Double valorTotal;
-    @Schema(description = "Desconto aplicado no item %", example = "10.00")
-    private Double desconto;
-    @Schema(description = "Valor do desconto aplicado no item R$", example = "10.00")
-    private Double valorDesconto;
-    @Schema(description = "Valor do frete aplicado no item", example = "10.00")
-    private Double valorFrete;
-    @Schema(description = "Custo de produção do item ao concluir confecção", example = "10.00")
-    private Double custoProducao;
-    @Schema(description = "Responsável confeccionou o produto", example = "true")
-    private Boolean feito;
-    private ProdutoResponseDTO produto;
-    private List<PersonalizacaoItemPedidoResponseDTO> personalizacoes;
+  private Integer id;
+  @Schema(description = "Quantidade de produtos", example = "10")
+  private Integer quantidade;
+  @Schema(description = "Valor unitário do produto", example = "10.00")
+  private Double valor;
+  @Schema(description = "Valor pago por este item com frete, desconto, valor unitário * qauntidade + custo da personalizacao", example = "100.00")
+  private Double valorTotal;
+  @Schema(description = "Desconto aplicado no item %", example = "10.00")
+  private Double desconto;
+  @Schema(description = "Valor do desconto aplicado no item R$", example = "10.00")
+  private Double valorDesconto;
+  @Schema(description = "Valor do frete aplicado no item", example = "10.00")
+  private Double valorFrete;
+  @Schema(description = "Custo de produção do item ao concluir confecção", example = "10.00")
+  private Double custoProducao;
+  @Schema(description = "Responsável confeccionou o produto", example = "true")
+  private Boolean feito;
+  private ProdutoResponseDTO produto;
+  private List<PersonalizacaoItemPedidoResponseDTO> personalizacoes;
 }
